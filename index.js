bbb544d73342ed312b48add55f11a0638898f9d4
IGOR <qrdej31@gmail.com>
2022-03-01 12:30:52 +0100

    ref

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 3001;
 
 app.get('/motorcycles', (req, res) => {
     db
-        .query("SELECT * FROM motorcycle")
+        .query("SELECT * FROM motorcycles")
         .then(result => res.send(result))
         .catch(err => console.log(err));
 });
